24d1033059eaae470ea40f9b831edf04534a23fe
Luarx <22997139+luarx@users.noreply.github.com>
2018-08-29 15:37:47 +0200

    Redirect authenticated users when access to login page

--- releasesmonitor/releasesmonitor/urls.py
+++ releasesmonitor/releasesmonitor/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, re_path, path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     re_path(r'^', include('projectsmanagement.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
