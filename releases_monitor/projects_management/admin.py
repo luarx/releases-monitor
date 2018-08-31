1553ea2ad47ed0b273b2a8ea8b5b3e7301a1a217
Luarx <22997139+luarx@users.noreply.github.com>
2018-08-31 13:38:05 +0200

    Create new db table for projects environments

--- releases_monitor/projects_management/admin.py
+++ releases_monitor/projects_management/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Project, Library, ProjectLibrary
+from .models import Project, Library, ProjectLibrary, Environment
 
 admin.site.register(Project)
 admin.site.register(Library)
 admin.site.register(ProjectLibrary)
+admin.site.register(Environment)
--- releases_monitor/projects_management/models.py
+++ releases_monitor/projects_management/models.py
@@ -4,22 +4,19 @@ from django.utils import timezone
 import re
 
 
-class Project(models.Model):
-    DEV = 'DEV'
-    STAGING = 'STAGING'
-    RELEASE = 'RELEASE'
-    PRO = 'PRO'
-    ENVIRONMENT_CHOICES = (
-        (DEV, 'Development'),
-        (STAGING, 'Staging'),
-        (RELEASE, 'Release'),
-        (PRO, 'Production'),
-    )
+class Environment(models.Model):
+    name = models.CharField(max_length=15)
+    verbose_name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     repo_url = models.URLField(max_length=200, blank=True)
-    environment = models.CharField(max_length=20, choices=ENVIRONMENT_CHOICES)
+    environment = models.ForeignKey(Environment, on_delete=models.CASCADE, null=True)
     creation_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
