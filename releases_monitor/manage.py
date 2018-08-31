7813734ba3e95aa12d25eac647378c3b43b3c5b3
Luarx <22997139+luarx@users.noreply.github.com>
2018-08-31 10:15:40 +0200

    Rename project name

--- releases_monitor/manage.py
+++ releases_monitor/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'releasesmonitor.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'releases_monitor.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
