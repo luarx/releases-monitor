7c1ade0cf0e4bb7ad6fa80879a0485552729204b
Luarx <22997139+luarx@users.noreply.github.com>
2019-05-07 12:54:36 +0200

    Add `release date` field to database

--- releases_monitor/projects_management/tables.py
+++ releases_monitor/projects_management/tables.py
@@ -6,14 +6,15 @@ class ProjectLibraryTable(tables.Table):
     projectName = tables.Column(accessor='project.name', verbose_name='Project')
     projectEnvironment = tables.Column(accessor='project.environment')
     lastVersion = tables.Column(accessor='library.last_version')
-    versionCheckDate = tables.Column(accessor='library.version_check_date')
+    lastVersionDate = tables.DateColumn(accessor='library.last_version_date')
+    versionCheckDate = tables.DateColumn(accessor='library.version_check_date')
 
     class Meta:
         model = ProjectLibrary
         template_name = "django_tables2/bootstrap.html"
         exclude = ("id", "creation_date", "project")
         sequence = ('projectName', 'projectEnvironment', 'library',
-                    'current_version', 'lastVersion', 'versionCheckDate', '...')
+                    'current_version', 'lastVersion', 'lastVersionDate', '...')
         row_attrs = {
             'class': lambda record: 'version-updated' if record.is_version_updated == True else ('version-outdated' if record.is_version_updated == False else 'library-last-version-not-synchronized')
         }
