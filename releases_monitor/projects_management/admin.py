57287646c31e8039bfe1f1ea7e028f96720c0d89
Luarx <renol_siete@hotmail.com>
2019-05-13 14:05:03 +0200

    Improve method to check if a library is updated
    If Patch version has to be checked, mayor and minor are also required to be checked

--- releases_monitor/projects_management/admin.py
+++ releases_monitor/projects_management/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Project, Library, ProjectLibrary, Environment
 
+class ProjectLibraryAdmin(admin.ModelAdmin):
+    class Media:
+        js = ('js/admin/project_library/checkbox-version-numbers.js', )
+
 admin.site.register(Project)
 admin.site.register(Library)
-admin.site.register(ProjectLibrary)
+admin.site.register(ProjectLibrary, ProjectLibraryAdmin)
 admin.site.register(Environment)
