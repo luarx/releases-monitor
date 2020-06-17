ee3d5334a521d9abf110ee6b5f47c8d44ba60ba7
Luarx <22997139+luarx@users.noreply.github.com>
2020-06-17 13:36:58 +0200

    Add more info to some admin interface tables

--- releases_monitor/projects_management/admin.py
+++ releases_monitor/projects_management/admin.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from .models import Project, Library, ProjectLibrary, Environment
 
 class ProjectLibraryAdmin(admin.ModelAdmin):
+    list_display = ['project', 'library', 'current_version']
+
     class Media:
         js = ('js/admin/project_library/checkbox-version-numbers.js', )
 
+@admin.register(Library)
+class LibraryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'last_version', 'last_version_date']
+
 admin.site.register(Project)
-admin.site.register(Library)
 admin.site.register(ProjectLibrary, ProjectLibraryAdmin)
 admin.site.register(Environment)
